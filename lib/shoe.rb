6719967484c923d767ef0b7e1a1acf548bbb1692
Percy-gif <percymajor@gmail.com>
2020-09-20 16:41:24 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,20 +4,26 @@ class Shoe
 
     BRANDS = []
 
-  # def initialize(brand)
-  #   @brand = brand
-  # end
+  def initialize(brand)
+    @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand) 
+    
+    # brands = ['Nike', 'Reebox', 'Nike', 'Kickers']
+
+  end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brand)
-    @brand = brand #NOTE - this seems redundant to line 8 above 
-    BRANDS << brand 
-  end 
+
 end
+
+  # def brand=(brand)
+  #   @brand = brand #NOTE - this seems redundant to line 8 above 
+
+  # end 
 
   #  # create the writer for genre and add the logic for the class constant
   #  def genre=(genre)
