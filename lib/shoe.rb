b19901b40b1f9637778d45d23bdf11415e27df94
Percy-gif <percymajor@gmail.com>
2020-09-20 15:47:05 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,6 +2,8 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+    BRANDS = []
+
   def initialize(brand)
     @brand = brand
   end
@@ -11,4 +13,14 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def brand=(brand)
+    @brand = brand #NOTE - this seems redundant to line 8 above 
+    BRANDS << brand 
+  end 
 end
+
+  #  # create the writer for genre and add the logic for the class constant
+  #  def genre=(genre)
+  #   @genre = genre
+  #   GENRES << genre 
+  # end
